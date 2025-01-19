563d6348982c96c965966ee7d841d6f5b7dbf004
SpaceCat2233 <yiwei.lin@student.tdsb.on.ca>
2025-01-19 13:27:44 -0500

    ToDoApp 1.0

---
+++ app/src/main/java/com/example/final30/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.final30
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.final30.RecyclerView.TaskAdapter
+import com.example.final30.ui.theme.Final30Theme
+
+class MainActivity : ComponentActivity() {
+    private val taskManager = TaskManager()
+    private lateinit var taskAdapter: TaskAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val taskInput = findViewById<EditText>(R.id.taskInput)
+        val addTaskButton = findViewById<Button>(R.id.addTaskButton)
+        val taskRecyclerView = findViewById<RecyclerView>(R.id.taskRecyclerView)
+
+        taskAdapter = TaskAdapter(taskManager.getTasksSortedByName()) {taskId ->
+            taskManager.completeTask(taskId)
+            updateTasks()
+        }
+
+        taskRecyclerView.layoutManager = LinearLayoutManager(this)
+        taskRecyclerView.adapter = taskAdapter
+
+        addTaskButton.setOnClickListener{
+            val taskName = taskInput.text.toString()
+            if (taskName.isNotBlank()){
+                taskManager.addTask(taskName)
+                taskInput.text.clear()
+                updateTasks()
+            }
+        }
+    }
+
+    private fun updateTasks(){
+        taskAdapter = TaskAdapter(taskManager.getTasksSortedByName()) { taskId ->
+            taskManager.completeTask(taskId)
+            updateTasks()
+        }
+        findViewById<RecyclerView>(R.id.taskRecyclerView).adapter = taskAdapter
+    }
+}
